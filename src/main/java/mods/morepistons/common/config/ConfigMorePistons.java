6c32f2150eb2c566d455c336c416ed12c351a511
Alireza Khodakarami <jiraiyahsama@yahoo.com>
2014-09-17 17:53:02 +0330

    my Initial commit on the branch

--- src/main/java/mods/morepistons/common/config/ConfigMorePistons.java
+++
@@ -1,8 +0,0 @@
-package mods.morepistons.common.config;
-
-import mods.gollum.core.common.config.Config;
-import mods.gollum.core.common.config.ConfigProp;
-
-public class ConfigMorePistons extends Config {
-	
-}
